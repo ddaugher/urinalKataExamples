538b3c02c5b360b577086860181fd24b187947f2
DJ Daugherty <ddaugher@gmail.com>
2015-04-01 19:25:15 -0500

    should return urinal position 3 when three urinals exists and position 2 is occupied

--- src/test/groovy/Bathroom_3Urinals_Spec.groovy
+++ src/test/groovy/Bathroom_3Urinals_Spec.groovy
@@ -22,4 +22,15 @@ public class Bathroom_3Urinals_Spec extends Specification {
     bathroom.nextAvailable() == bathroom.urinals.get(0)
     1 == bathroom.urinals.get(0).position
   }
+
+  def "should return urinal position 3 when three urinals exists and position 2 is occupied"() {
+    given: "instantiated Urinal"
+    def bathroom = new Bathroom(3)
+    bathroom.urinals.get(1).status = UrinalStatus.OCCUPIED
+
+    expect: "position 3 should be returned"
+    bathroom.numberOfUrinals == 3
+    bathroom.nextAvailable() == bathroom.urinals.get(2)
+    3 == bathroom.urinals.get(2).position
+  }
 }
