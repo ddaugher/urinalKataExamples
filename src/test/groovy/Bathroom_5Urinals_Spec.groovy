0a2f93af943f29a624373740b66be2b097723af8
DJ Daugherty <ddaugher@gmail.com>
2015-04-01 20:47:00 -0500

    handle 4 and 5 urinals

---
+++ src/test/groovy/Bathroom_5Urinals_Spec.groovy
@@ -0,0 +1,30 @@
+import spock.lang.Specification
+
+public class Bathroom_5Urinals_Spec extends Specification {
+
+  def "should return urinal position 1 when five urinals exists and 2, 3 and 5 are occupied"() {
+    given: "instantiated Urinal"
+    def bathroom = new Bathroom(5)
+    bathroom.urinals.get(1).status = UrinalStatus.OCCUPIED
+    bathroom.urinals.get(2).status = UrinalStatus.OCCUPIED
+    bathroom.urinals.get(4).status = UrinalStatus.OCCUPIED
+
+    expect: "position 1 should be returned"
+    bathroom.numberOfUrinals == 5
+    bathroom.nextAvailable() == bathroom.urinals.get(0)
+    1 == bathroom.urinals.get(0).position
+  }
+
+  def "should return urinal position 1 when five urinals exists and 2, 4 and 5 are occupied"() {
+    given: "instantiated Urinal"
+    def bathroom = new Bathroom(5)
+    bathroom.urinals.get(1).status = UrinalStatus.OCCUPIED
+    bathroom.urinals.get(3).status = UrinalStatus.OCCUPIED
+    bathroom.urinals.get(4).status = UrinalStatus.OCCUPIED
+
+    expect: "position 1 should be returned"
+    bathroom.numberOfUrinals == 5
+    bathroom.nextAvailable() == bathroom.urinals.get(0)
+    1 == bathroom.urinals.get(0).position
+  }
+}
