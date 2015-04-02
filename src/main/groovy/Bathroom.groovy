d479561d4a899989f10453449084065f03b8bd54
DJ Daugherty <ddaugher@gmail.com>
2015-04-01 21:37:10 -0500

    refactor

--- src/main/groovy/Bathroom.groovy
+++ src/main/groovy/Bathroom.groovy
@@ -19,10 +19,10 @@ public class Bathroom {
   }
 
   private def createMultiUrinalFacility(int numberOfUrinals) {
-    return (1..numberOfUrinals).each { it ->
-      if (it == 1) createLeftUrinal()
-      if (it > 1 && it < numberOfUrinals) createMiddleUrinal(it)
-      if (it == numberOfUrinals) createRightUrinal(it)
+    return (1..numberOfUrinals).each { index ->
+      if (index == 1) createFirstUrinal()
+      if (index > 1 && index < numberOfUrinals) createMiddleUrinal(index)
+      if (index == numberOfUrinals) createLastUrinal(index)
     }
   }
 
@@ -34,28 +34,32 @@ public class Bathroom {
     return 0 == numberOfUrinals
   }
 
-  private createRightUrinal(int it) {
-    Urinal u = urinals.get(it - 1)
-    u.position = it
-    u.leftNeighbor = urinals.get(it - 2)
-    u.rightNeighbor = null
-  }
-
-  private createMiddleUrinal(int it) {
-    Urinal u = urinals.get(it - 1)
-    u.position = it
-    u.leftNeighbor = urinals.get(it - 2)
-    u.rightNeighbor = new Urinal()
-    urinals.add(u.rightNeighbor)
-  }
-
-  private createLeftUrinal() {
+  private createFirstUrinal() {
     def u = new Urinal()
     u.position = 1
     u.leftNeighbor = null
     u.rightNeighbor = new Urinal()
     urinals.add(u)
     urinals.add(u.rightNeighbor)
+  }
+
+  private createMiddleUrinal(int index) {
+    Urinal u = urinals.get(index - 1)
+    u.position = index
+    u.leftNeighbor = getLeftNeighbor(index)
+    u.rightNeighbor = new Urinal()
+    urinals.add(u.rightNeighbor)
+  }
+
+  private Object getLeftNeighbor(int index) {
+    return urinals.get(index - 2)
+  }
+
+  private createLastUrinal(int index) {
+    Urinal u = urinals.get(index - 1)
+    u.position = index
+    u.leftNeighbor = getLeftNeighbor(index)
+    u.rightNeighbor = null
   }
 
   private createSingleUrinal() {
@@ -73,16 +77,18 @@ public class Bathroom {
   def nextAvailable() {
     if (urinals.isEmpty()) return null
 
-    def available = urinals.findAll().findAll {it.isAvailable()}
-    if (available.size() == 0) {
-      def a = urinals.findAll().findAll {it.status == UrinalStatus.AVAILABLE}
-      if (!a.isEmpty()) {
-        return a.last()
-      } else {
-        return null
-      }
-    }
+    def availableWithoutNeighbors = gatherAvailableWithoutNeighbors()
 
-    available.last()
+    availableWithoutNeighbors.size() > 0 ? availableWithoutNeighbors.last() : availableWithNeighbors()
+
+  }
+
+  private def availableWithNeighbors() {
+    def available = urinals.findAll().findAll { it.status == UrinalStatus.AVAILABLE }
+    available.isEmpty() ? null : available.last()
+  }
+
+  private def gatherAvailableWithoutNeighbors() {
+    return urinals.findAll().findAll { it.isAvailable() }
   }
 }
